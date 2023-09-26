ccf8df2f7920c53b932e3a51d1de32a9ad9a808b
Praveen-Meena <singhjimeena123@gmail.com>
2023-09-26 22:00:20 +0530

    My Discord Clone

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["*"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+  fontfamily:{
+    Whitney:["whitney"],
+    Ginto:["ginto"], 
+    ggSans:["ggSans"],
+  }
+}
+
